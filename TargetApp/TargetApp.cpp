e98c7401c73f57b13253e3a6a98fe493e5a51bbf
Pavel P <pavlov.pavel@gmail.com>
2023-03-12 22:36:23 +0300

    Support Symbol0 and Symbol1
    Switch to use Symbol2::toStringImpl

--- TargetApp/TargetApp.cpp
+++ TargetApp/TargetApp.cpp
@@ -9,9 +9,21 @@
 
 int wmain(int argc, WCHAR* argv[])
 {
-    Symbol2 sym2_spy("spy");
-    Symbol2 sym2_nylda("NYLD.A");
-    Symbol2 sym2_unknown;
+    Symbol0* sym2_spy0 = new Symbol0("spy");
+    Symbol0 sym2_nylda0("NYLD.A");
+    Symbol0 sym2_unknown0;
+
+    Symbol1* sym2_spy1 = new Symbol1("spy");
+    Symbol1 sym2_nylda1("NYLD.A");
+    Symbol1 sym2_unknown1;
+
+    Symbol2* sym2_spy2 = new Symbol2("spy");
+    Symbol2 sym2_nylda2("NYLD.A");
+    Symbol2 sym2_unknown2;
+
+    std::vector<Symbol0> syms0 = { Symbol0("SPY"), Symbol0("aapl") };
+    std::vector<Symbol1> syms1 = { Symbol1("SPY"), Symbol1("aapl") };
+    std::vector<Symbol2> syms2 = { Symbol2("SPY"), Symbol2("aapl") };
 
     __debugbreak(); // program will stop here. Evaluate 'sym2_spy', 'sym2_nylda' and 'sym2_unknown' in the locals or watch window.
     std::cout << "Test complete\n";
